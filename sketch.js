d120957c2bf33911aa25269f1939d39b8b6b16df
Jake Hamill <hamilljake1@gmail.com>
2016-11-28 20:46:27 -0600

    Final

--- sketch.js
+++ sketch.js
@@ -1,56 +1,91 @@
-var time1 = 2000;
-var time2 = 30000;
-
 var angle = 0;
-var offset = 30;
-var scalar = 1;
-var speed = .5;
+var offset = 1.5;
+var scalar = 1.5;
+var speed = .09;
 var r = 0;
 
-var backg;
 var blue;
 var pink;
 var shape;
+var cloth;
+var fur;
+var gold;
+var ice;
+var teal;
 
+var music;
+var actualTime = 0;
+var loadTime = 0;
 
-
-//var music;
 
 function preload(){
-
-   //music = loadSound("music.mp3");
-
-   backg = loadImage("imgs/background.png");
    blue = loadImage("imgs/blue.png");
    pink = loadImage("imgs/pink.png");
    shape = loadImage("imgs/shape.png");
+   cloth = loadImage("imgs/cloth.png");
+   fur = loadImage("imgs/fur.png");
+   gold = loadImage("imgs/gold.png");
+   ice = loadImage("imgs/ice.png");
+   teal = loadImage("imgs/teal.png");
+
 }
 
 function setup() {
   createCanvas(1000,700);
-  imageMode(CENTER);
-  image(backg, width/2,height/2);
+  music = loadSound("music/music.mp3", playMusic);
 
+}
 
-  //music.loop();
-
+function playMusic() {
+  loadTime = millis();
+  music.loop();
 }
 
 function draw(){
-var currentTime = millis();
-if (currentTime > time1) {
-  var x = offset + cos(angle) * scalar;
-  var y = offset + sin(angle) * scalar;
-  rotate(r);
-  r = r + .01;
-  image(pink, x, y);
-  image(shape, x, y);
-  angle = angle + speed;
-  scalar = scalar + speed;
-}
-if (currentTime > time2) {
-  x = x + 2;
-   image(blue, x, y, width/4, height/4);
- }
+  actualTime = millis() - loadTime;
 
+
+  if (actualTime > 0 && actualTime < 164000) {
+    var x = offset + cos(angle) * scalar;
+    var y = offset + sin(angle) * scalar;
+
+    translate(500, 350);
+    rotate(r);
+    r = r + .01;
+    image(pink, x, y);
+    image(shape, x, y);
+    angle = angle + speed;
+    scalar = scalar + speed;
+
+  }
+  if (actualTime > 0 && actualTime < 164000) {
+    translate(500, 350);
+    rotate(r);
+    r = r + .005;
+    image(cloth, x, y);
+  }
+  if (actualTime > 500 && actualTime < 164000) {
+    translate(500, 350);
+    rotate(r);
+    r = r + .01;
+    image(fur, x, y);
+  }
+  if (actualTime > 2000 && actualTime < 164000) {
+    translate(500, 350);
+    rotate(r);
+    r = r + .01;
+    image(gold, x, y);
+  }
+  if (actualTime > 3700 && actualTime < 164000) {
+    translate(500, 350);
+    rotate(r);
+    r = r + .01;
+    image(ice, x, y);
+  }
+  if (actualTime > 5000 && actualTime < 164000) {
+    translate(500, 350);
+    rotate(r);
+    r = r + .01;
+    image(teal, x, y);
+  }
 }
